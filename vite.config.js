878493976a1c71f21d10c973017a52ac3ea0d3c7
okitomomo <t.okita.41119@gmail.com>
2025-11-26 16:14:42 +0900

    deployで起こる不具合を修正

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './',
-  base: '/Mahjong/',
+  base: '/mahjong/',
   server: {
     host: true,    // コンテナ外からアクセス可能に
     watch: {
